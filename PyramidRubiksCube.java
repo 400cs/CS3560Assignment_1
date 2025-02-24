eb8cf132a23500ed5abd773ace091e52f610ccdf
400cs <78524100+400cs@users.noreply.github.com>
2025-02-23 21:08:20 -0800

    first commit

---
+++ PyramidRubiksCube.java
@@ -0,0 +1,20 @@
+public class PyramidRubiksCube {
+    
+
+    // pick data struc
+
+    //should set up the cube’s edges, ensuring proper placement of tiles and their colors
+    public initializeEdges() {
+
+    }
+    //should initialize the faces of the pyramid, ensuring the correct structure is followed
+    public initializeFaces() {
+        
+    }
+
+    //Verify that each face contains exactly 9 tiles
+    //Ensure that the color distribution across the cube follows the valid rules
+    public validateCube() {
+
+    }
+}
